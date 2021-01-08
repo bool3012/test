cc65ea0920eb9e454f16cd7d491b788cbfe71328
REPLYNET\a.zannetti <a.zannetti@reply.it>
2021-01-08 13:00:44 +0100

    remove ec2

--- main.tf
+++ main.tf
@@ -23,24 +23,24 @@ provider "aws" {
   }
 }
 
-module "my_ec2" {
-  source                 = "./modules/aws-ec2"
-  #version                = "~> 2.0"
-  name                   = "my-ec2"
-  ami                    = "ami-01720b5f421cf0179"
-  instance_type          = "t2.micro"
-  ssh_key                = "azannetti"
-  enable_monitoring      = false
-  public                 = false
-  security_groups        = ["sg-042b175d17b5c31d6"]
-  subnet_ids             = ["subnet-036a9578fadf469d9"]
-  vpc_id                 = "vpc-0c8f0856e8b325c59"
-  ssh_source_restriction = "sg-042b175d17b5c31d6"
-  allow_icmp = true
-  tags = merge(
-    var.common_tags,
-    {
-      "Environment" = "LAB"
-    },
-  )
-}
+#module "my_ec2" {
+#  source                 = "./modules/aws-ec2"
+#  #version                = "~> 2.0"
+#  name                   = "my-ec2"
+#  ami                    = "ami-01720b5f421cf0179"
+#  instance_type          = "t2.micro"
+#  ssh_key                = "azannetti"
+#  enable_monitoring      = false
+#  public                 = false
+#  security_groups        = ["sg-042b175d17b5c31d6"]
+#  subnet_ids             = ["subnet-036a9578fadf469d9"]
+#  vpc_id                 = "vpc-0c8f0856e8b325c59"
+#  ssh_source_restriction = "sg-042b175d17b5c31d6"
+#  allow_icmp = true
+#  tags = merge(
+#    var.common_tags,
+#    {
+#      "Environment" = "LAB"
+#    },
+#  )
+#}
